56ceef2abd6296dced8e38149b536ff454d95632
Mohamed Atef <64927903+atef2827@users.noreply.github.com>
2020-05-07 01:28:34 +0200

    lg & header & footer & first & second row

--- redhorn/src/app/app-routing.module.ts
+++ redhorn/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// pages 
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CapabilitiesComponent } from './pages/capabilities/capabilities.component';
+import { WorkComponent } from './pages/work/work.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: HomepageComponent },
+  { path: 'capabilities' , component: CapabilitiesComponent },
+  { path: 'work' , component: WorkComponent },
+  { path: 'contact-us' , component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- redhorn/src/app/app.module.ts
+++ redhorn/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CapabilitiesComponent } from './pages/capabilities/capabilities.component';
+import { WorkComponent } from './pages/work/work.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    CapabilitiesComponent,
+    WorkComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
