3ee2c794e4eaac691f2d70ada9808b4f113333d1
Mohamed Atef <atef2827@gmail.com>
2020-05-09 01:26:26 +0200

    Few edit in Capabilities

--- redhorn/src/app/layouts/header/header.component.ts
+++ redhorn/src/app/layouts/header/header.component.ts
@@ -10,6 +10,14 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // document.addEventListener('scroll', function() {
+    //   if(document.body.scrollTop > 100){
+    //     console.log('sticky');
+    //   }
+    //   if(document.body.scrollHeight == 0){
+    //     console.log('top');
+    //   }
+    // });
   }
 
 }
