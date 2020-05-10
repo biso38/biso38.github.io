03946953797e1a3a41c553787e572f9d1e85b8c6
Mohamed Atef <atef2827@gmail.com>
2020-05-10 02:20:30 +0200

    adjusting scroll to top

--- redhorn/src/app/common/common.service.ts
+++ redhorn/src/app/common/common.service.ts
@@ -40,7 +40,11 @@ export class CommonService {
       if (!(evt instanceof NavigationEnd)) {
           return;
       }
-      window.scrollTo(0, 0)
+      window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth'
+      });
     });
   }
 }
