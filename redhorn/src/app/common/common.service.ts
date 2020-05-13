5c3c2574c7af53ff16a0b992e4bc09cc424e9112
biso38 <x_123@windowslive.com>
2020-05-14 01:24:01 +0200

    Revert "Remote code updates"

--- redhorn/src/app/common/common.service.ts
+++ redhorn/src/app/common/common.service.ts
@@ -7,15 +7,7 @@ import { Router, NavigationEnd } from '@angular/router';
 export class CommonService {
 
   constructor( private router: Router) { }
-
   observer : any ; 
-
-  config = {
-    root: null, 		// sets the framing element to the viewport
-    rootMargin: '0px',
-    threshold: 0.5
-  };
-
   page_options(options : any){
     let BG = options.HeaderBG ;
     let StickyBG = options.StickyBG ;
@@ -48,16 +40,16 @@ export class CommonService {
       }
       scroll(0,0);
     });
-    // this.animation_function_left();
-    // this.animation_function_right();
-    // this.animation_function_FromBottom();
-    // this.animation_function_FromTop();
+    this.animation_function_left();
+    this.animation_function_right();
+    this.animation_function_FromBottom();
+    this.animation_function_FromTop();
   }
   animation_function_left(){
     const images = document.querySelectorAll('.fromleftfadeanimation');
     this.observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
-            console.log(entry);
+            // console.log(entry);
             if(entry.intersectionRatio > 0) {
                 if(!entry.target.classList.contains('done_animate')){
                     entry.target.classList.add('done_animate');
@@ -68,7 +60,7 @@ export class CommonService {
             //   (<HTMLElement>entry.target).style.animation = 'none';
             // }
         });
-    }, );
+    });
     images.forEach(image => {
         this.observer.observe(image);
     });
@@ -85,7 +77,7 @@ export class CommonService {
                 }
             }
         });
-    }, );
+    });
     images.forEach(image => {
         this.observer.observe(image);
     });
@@ -102,7 +94,7 @@ export class CommonService {
                 }
             }
         });
-    }, );
+    });
     images.forEach(image => {
         this.observer.observe(image);
     });
@@ -119,7 +111,7 @@ export class CommonService {
                 }
             }
         });
-    }, );
+    });
     images.forEach(image => {
         this.observer.observe(image);
     });
