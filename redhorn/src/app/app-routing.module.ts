9ad9e10b8fbb18cf958030520baed7a5911263c5
Mohamed Atef <atef2827@gmail.com>
2020-05-20 01:53:43 +0200

    Update menu and add case study page

--- redhorn/src/app/app-routing.module.ts
+++ redhorn/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { CapabilitiesComponent } from './pages/capabilities/capabilities.component';
 import { WorkComponent } from './pages/work/work.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { CaseStudyComponent } from './pages/case-study/case-study.component';
 
 const routes: Routes = [
   { path: '' , component: HomepageComponent },
   { path: 'capabilities' , component: CapabilitiesComponent },
   { path: 'work' , component: WorkComponent },
   { path: 'contact-us' , component: ContactUsComponent },
+  { path: 'case-study/:slug' , component: CaseStudyComponent },
 ];
 
 @NgModule({
